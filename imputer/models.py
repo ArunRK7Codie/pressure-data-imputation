729d8db85078cb74c0aed82b32a2a2c5429edc11
Arun Pandian R <smartboyarun56@gmail.com>
2020-06-05 22:54:38 +0530

    Bug Fixes

--- imputer/models.py
+++ imputer/models.py
@@ -13,26 +13,29 @@ iterations=1000
 
 
 def knn_impute(df,k) -> pd.DataFrame():
+    print("Imputing with KNN")
     imputed_ls = list() # for storing imputed time series record
-    for i in tqdm(range(df[0].size)): # Iterate through n time series values in df
+    for i in tqdm(range(df[3].size)): # Iterate through n time series values in df
         row_val = df.iloc[i,:] #selects one time series for imputation
-        row_np = row_val.values().reshape(10, 5) # reshapes a one dimensional row of values into 2d(10x5) matrix for imputing 
+        row_np = row_val.values.reshape(10, 5) # reshapes a one dimensional row of values into 2d(10x5) matrix for imputing 
         imputed_row = impy.fast_knn(row_np, k=k).reshape(1,50) #Implements KNN Imputation functions from impyute module
         imputed_ls.append(imputed_row.tolist()) 
-    imputed_df = pd.DataFrame(imputed_ls[i][0] for i in range(df[0].shape)) # turns into Dataframe object again
+    imputed_df = pd.DataFrame(imputed_ls[i][0] for i in range(df[3].size)) # turns into Dataframe object again
     return imputed_df.copy()
 
 def mice_impute(df) -> pd.DataFrame():
+    print("Imputing with MICE")
     imputed_ls = list() # for storing imputed time series record
     for i in tqdm(range(df[0].size)): # Iterate through n time series values in df
         row_val = df.iloc[i,:] #selects one time series for imputation
-        row_np = row_val.values().reshape(10, 5) # reshapes a one dimensional row of values into 2d(10x5) matrix for imputing 
+        row_np = row_val.values.reshape(10, 5) # reshapes a one dimensional row of values into 2d(10x5) matrix for imputing 
         imputed_row = impy.mice(row_np).reshape(1,50) #Implements MICE Imputation functions from impyute module
         imputed_ls.append(imputed_row.tolist()) 
-    imputed_df = pd.DataFrame(imputed_ls[i][0] for i in range(df[0].shape)) # turns into Dataframe object again
+    imputed_df = pd.DataFrame(imputed_ls[i][0] for i in range(df[3].size)) # turns into Dataframe object again
     return imputed_df.copy()
 
 def gain_impute(df) -> pd.DataFrame():
+    print("Imputing with GAIN")
     gain_parameters = {'batch_size': batch_size,
                      'hint_rate': hint_rate,
                      'alpha': alpha,
